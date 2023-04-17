69d3a1109fda7b9a79c73eaa8e1bab8f1aa5a334
Mateusz Zerebiec <43859749+ZerebiecMateusz@users.noreply.github.com>
2023-04-17 20:03:41 +0200

    ready switch

--- src/conteiners/ToDoList.js
+++ src/conteiners/ToDoList.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 
 import Todo from "../components/Todo";
 import { toggleTodo } from "../actions/todo.actions";
+import { AVAILABLE_FILTERS } from "../actions/todo.actions";
+import filter from "../reducers/filter.reducer";
 
 const TodoList = ({ todos, toggleTodo }) => {
     return (
@@ -14,8 +16,20 @@ const TodoList = ({ todos, toggleTodo }) => {
     );
 };
 
+const filterTodos = (todos, filter) => {
+    switch(filter) {
+        case AVAILABLE_FILTERS.SHOW_COMPLETE:
+        return todos.filter(todo => todo.complete === true)
+        case AVAILABLE_FILTERS.SHOW_ACTIVE:
+        return todos.filter(todo => todo.complete === false)
+
+        default:
+            return todos;
+    }
+}
+
 const mapStateToProps = state => ({
-    todos: state.todos
+    todos: filterTodos(state.todos, state.filter)
 })
 
 const mapDispatchToProps = {
