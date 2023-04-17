367615303537aa7aed39ae67f433d64023329c47
Mateusz Zerebiec <43859749+ZerebiecMateusz@users.noreply.github.com>
2023-04-17 21:51:56 +0200

    Middleware Logger

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit'
+// import { configureStore, applyMiddleware } from '@reduxjs/toolkit'
+import { configureStore} from '@reduxjs/toolkit';
 import reducers from './reducers'
 
+// middlewares redux (logger)
+//funkcja zwróci funkcje (i to zapewnia nam redux)
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info("dispatching", action)
+  let result = next(action);
+  console.log("next state", store.getState());
+  console.groupEnd();
+
+  return result;
+};
 
 // const store = configureStore({rootReducer})
-const store = configureStore(
-  {reducer:reducers}
-  );
+// const store = configureStore(
+//   {reducer:reducers},
+//   applyMiddleware(logger)
+//   );
+
+// this worked with configureStore (old version is CreateStore )
+const store = configureStore({
+  reducer:reducers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
